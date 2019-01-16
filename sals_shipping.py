c46a21a7a791c22fac51555a77d8de6ab98212bc
farouk kenny fakunle <farouk.fakunle@gmail.com>
2019-01-15 20:36:51 -0500

    Function for drone shipping costs added.

--- sals_shipping.py
+++ sals_shipping.py
@@ -11,4 +11,16 @@ def normal_ground_shipping_cost(weight):
   else:
     return weight * 4.75 + 20
 
+def drone_shipping_cost(weight):
+  if weight <= 2:
+    return weight * 4.50 + 0.00
+  elif weight > 2 and weight <= 6:
+    return weight * 9.00 + 0.00
+  elif weight > 6 and weight <= 10:
+    return weight * 12.00 + 0.00
+  else:
+    return weight * 14.25 + 0.00 
+
+#test the code below with the print function
 #print(normal_ground_shipping_cost(8.4))
+#print(drone_shipping_cost(1.5))
